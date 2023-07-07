ad13fe078a626bafffe8a29d4049c57144513845
Junnel Padilla <junnelpadilla307@gmail.com>
2023-07-07 00:54:55 -0700

    added a header and navigation bar to portfolio

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header-style'>
+        <h1>JP Padilla</h1>
+        <nav className='nav-dashboard'>
+          <a href='/'>About Me</a>
+          <a href='/'>Portfolio</a>
+          <a href='/'>Contacts</a>
+          <a href='/'>Resume</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
